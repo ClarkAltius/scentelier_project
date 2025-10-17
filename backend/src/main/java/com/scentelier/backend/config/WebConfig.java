5cd630e840fd628f857b536a7f8cba3d508d61f7
wonjinKim <wonjin.kim0911@gmail.com>
2025-10-17 17:55:30 +0900

    application.properties 수정, WebConfig.java 추가

---
+++ backend/src/main/java/com/scentelier/backend/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.scentelier.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${uploadPath}")
+    private String uploadPath;
+
+    @Override // "/images/**" 요청시, uploadPath
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations(uploadPath);
+    }
+}
