edadbf5f92f93ae8bee5a5619543010be0a25ec3
ClarkAltius <ilhwana@gmail.com>
2025-10-17 15:39:09 +0900

    Reapply "Merge branch 'main' of https://github.com/ClarkAltius/scentelier_project"
    This reverts commit 80d5c07bd12403e2f0d7c103e28c432997cec2d5.

---
+++ frontend/src/ui/MenuItems.js
@@ -0,0 +1,27 @@
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+function MenuItems() {
+
+    const navigate = useNavigate();
+
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href='/'>Scentelier</Navbar.Brand>
+                <Navbar.Brand>임시</Navbar.Brand>
+                <Nav className="me-auto">
+                    <NavDropdown title={`1차 페이지 작성`}>
+                        <NavDropdown.Item onClick={() => navigate(`/admin`)}>관리자 랜딩 페이지</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => { }}>로그인 페이지</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => { }}>상품 상세 페이지</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => { }}>장바구니 페이지</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => { }}>결제 페이지</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default MenuItems;
