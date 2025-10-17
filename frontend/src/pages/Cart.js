541a4874fe1501522fcd65eda56c84307845dcf2
allright <allright@example.com>
2025-10-17 17:08:55 +0900

    test

---
+++ frontend/src/pages/Cart.js
@@ -0,0 +1,15 @@
+function Cart() {
+
+    return (
+        <div>
+            <header className="App-Header">
+                <p>
+                    장바구니 페이지
+                </p>
+            </header>
+        </div>
+
+    );
+}
+
+export default Cart;
--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -3,6 +3,7 @@ import Admin from "../pages/Admin.js";
 import Home from "../pages/Home.js";
 import Payments from "../pages/Payments.js";
 import Login from "../pages/Login.js";
+import Cart from "../pages/Cart.js";
 
 function AppRoutes() {
 
@@ -12,6 +13,7 @@ function AppRoutes() {
             <Route path='/admin' element={<Admin />} />
             <Route path='/payments' element={<Payments />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/cart' element={<Cart />} />
         </Routes>
     );
 
