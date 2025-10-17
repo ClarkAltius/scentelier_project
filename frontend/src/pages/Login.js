07577cf0d80117fff2abc38cd213448e44fb9af3
ClarkAltius <ilhwana@gmail.com>
2025-10-17 15:47:56 +0900

    package-lock.json 파일 재생성

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,15 @@
+function Admin() {
+
+    return (
+        <div>
+            <header className="App-Header">
+                <p>
+                    Login 기능 페이지
+                </p>
+            </header>
+        </div>
+
+    );
+}
+
+export default Admin;
