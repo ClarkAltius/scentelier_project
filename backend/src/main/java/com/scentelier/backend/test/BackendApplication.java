a5676710d6fd9b3881afb96bf4c5620d7128f8d3
ClarkAltius <ilhwana@gmail.com>
2025-10-17 11:53:55 +0900

    Folder Structure

--- backend/src/main/java/com/scentelier/backend/test/BackendApplication.java
+++ backend/src/main/java/com/scentelier/backend/test/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.scentelier.backend;
+package com.scentelier.backend.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
