e8e6ea716e13a90e37ac8ea9d17a0c52401bedc2
wonjinKim <wonjin.kim0911@gmail.com>
2025-10-20 13:12:34 +0900

    Create CustomPerfume.java

---
+++ backend/src/main/java/com/scentelier/backend/entity/CustomPerfume.java
@@ -0,0 +1,42 @@
+package com.scentelier.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Getter @Setter @ToString
+@Entity @Table(name = "custom_perfume")
+public class CustomPerfume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "custom_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users users;
+
+    @NotBlank(message = "향수 이름을 정해주세요.")
+    private String name;
+
+    @NotBlank(message = "용량을 정해 주세요.")
+    private int volume;
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
+
+    @Column(name = "is_deleted", columnDefinition = "TINYINT(1) DEFAULT 0")
+    private boolean isDeleted = false;
+
+    @Column(name = "deleted_at", columnDefinition = "DATETIME DEFAULT NULL")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate deletedAt;
+}
