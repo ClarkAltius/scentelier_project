5fb1a8a30826ddf5dccb147266ea49e503541db8
dwa <apfhstkxkd77@naver.com>
2025-10-20 13:14:26 +0900

    상품(커스텀제외)엔티티작성

---
+++ backend/src/main/java/com/scentelier/backend/entity/Ingredient.java
@@ -0,0 +1,52 @@
+package com.scentelier.backend.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "ingredient")
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ingredient_id")
+    private Long id;
+
+    @Column(nullable = false)//값입력필수
+    private String name;
+
+    @Column(nullable = false,length = 1000)//값입력필수
+    @NotBlank(message = "상품설명은 필수입력 사항입니다.")
+    @Size(max = 1000, message = "상품에 대한 설명은 최대 1000자리 이하로 입력해주세요")
+    private String  description;
+
+    @Column(nullable = false)//값입력필수
+    @Min(value = 10, message = "재고 수량은 10개 이상이여야합니다.")
+    @Max(value = 300, message = "재고 수량은 300개 이하여야합니다.")
+    private int stock;
+
+    @Column(name = "is_deleted", columnDefinition = "TINYINT(1) DEFAULT 0")
+    private boolean isDeleted;
+
+    @Column(name = "deleted_at", columnDefinition = "DATETIME DEFAULT NULL")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate deletedAt;
+
+    @Column(name = "img_url")
+    @NotBlank(message = "이미지는 필수입력 사항입니다.")
+    private String imgUrl ;
+
+}
---
+++ backend/src/main/java/com/scentelier/backend/entity/PerfumeTypeRecommendation.java
@@ -0,0 +1,33 @@
+package com.scentelier.backend.entity;
+
+import com.scentelier.backend.constant.Category;
+import com.scentelier.backend.constant.Note;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "perfume_type_recommendation")
+public class PerfumeTypeRecommendation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // 아이디를 자동 생성해줌
+    @Column(name = "recomend_id")
+    private Long id;
+
+    @Column(name = "perfume_type")
+    private String perfumeType ; //원액타입
+
+    @Column(name = "note_type")
+    @Enumerated(EnumType.STRING)
+    private Note noteType ; //노트
+
+    @ManyToOne(fetch = FetchType.LAZY) //일대일 연관 관계 매핑, 지연 로딩
+    @JoinColumn(name = "ingredient_id") // 외래키 지정
+    private Ingredient ingredients ; //향료아이디
+}
