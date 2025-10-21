479d90f52b3d863ba49cbeac5b7f40061140458f
wonjinKim <wonjin.kim0911@gmail.com>
2025-10-21 17:20:49 +0900

    상품 등록 폼 수정

--- backend/src/main/java/com/scentelier/backend/constant/Category.java
+++ backend/src/main/java/com/scentelier/backend/constant/Category.java
@@ -1,4 +1,15 @@
 package com.scentelier.backend.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
+    ALL("전체"), CITRUS("시트러스"), FLORAL("플로럴"), WOODY("우디"), CHYPRE("시프레"), GREEN("그린"), FOUGERE("푸제르"), ORIENTAL("오리엔탈"), AMBER("앰버");
+
+    private final String description;
+
+    Category(String description) {
+        this.description = description;
+    }
+
 }
